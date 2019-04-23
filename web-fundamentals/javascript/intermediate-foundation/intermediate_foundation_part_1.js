82ad7e46c0089571b5897a1e103be7b5210e524f
Matthew Bower <mbower386@gmail.com>
2019-04-23 14:02:51 -0700

    finished part 1 of intermediate fundamentals in javascript

--- web-fundamentals/javascript/intermediate-foundation/intermediate_foundation_part_1.js
+++ web-fundamentals/javascript/intermediate-foundation/intermediate_foundation_part_1.js
@@ -54,11 +54,55 @@ function second_to_last(arr) {
 }
 
 // #5:
-function n_to_last (arr, n) {
+function n_to_last(arr, n) {
   if (arr.length >= n) {
     return arr[arr.length - n];
   }
   else {
     return null;
   }
+}
+
+// #6:
+function second_largest(arr) {
+  var largest = arr[0];
+
+  if (arr.length < 2) {
+    largest = null;
+  }
+  else {
+    var index_of_largest = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[index_of_largest]) {
+        index_of_largest = i;
+      }
+    }
+
+    arr.splice(index_of_largest, 1);
+
+    largest = arr[0];
+
+    for (var j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[index_of_largest]) {
+        index_of_largest = j;
+      }
+    }
+
+    largest = arr[index_of_largest];
+  }
+
+  return largest;
+}
+
+// #7:
+function double_trouble(arr) {
+  var new_arr = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    new_arr.push(arr[i]);
+    new_arr.push(arr[i]);
+  }
+
+  return new_arr;
 }
